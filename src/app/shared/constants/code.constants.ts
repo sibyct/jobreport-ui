11e6f58f4342ad595af6b55123ca0022cf20bd25
sibyct <sibyct@gmail.com>
2020-05-31 18:53:16 +0200

    added constants

--- src/app/shared/constants/code.constants.ts
+++ src/app/shared/constants/code.constants.ts
@@ -3,7 +3,8 @@ import { InjectionToken } from '@angular/core';
 export const CODES = {
    AMC :42,
    VIP : 41,
-   OTHERS : 43 
+   OTHERS : 43,
+   OTHER_COMPLAINT:12
 }
 
 export const CODE_TOKEN = new InjectionToken<string>('codes');
